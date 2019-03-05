ecdc200ddab45ba9dccb035be0f737db1a72a000
Alexis Hessel <ahessel1998@gmail.com>
2019-03-04 19:53:34 -0700

    fixed image issues

--- pygamescode/Ascension.py
+++ pygamescode/Ascension.py
@@ -1,62 +1,21 @@
 import  os, sys, pygame, random
+import AsscensionLib, displaylib, eventhandle
 from pygame.locals import *
-
-
-class image:
-    def __init__(self, fileName):
-        self.fileName = fileName
-
-class Mountain:
-    def __init__(self, name, height, difficultyoverall, routelength, difficulties):
-        self.name = name
-        self.height = height
-        self.difficulty = difficultyoverall
-        self.routeLength = routelength
-        self.diff = difficulties
-
-
-class level:
-    def __init__(self, name):
-        self.name = name
-        self.mounts = []
-        self.images = []
-    def on_init(self):
-        try:
-            cwd  = os.getcwd()
-            with open(os.path.join(cwd, "mountains.txt"), "r") as f:
-                array = []
-                for line in f:
-                    array.append(line.rstrip('\n'))       
-        except:
-           print( "Could not load the level" )
-        for i in array:
-            info = (i.split(";"))
-            name = info[0]
-            height = info[1]
-            difficulty = info[2]
-            routelen = info[3]
-            diffs = info[4]
-            #diffs = [ x[0] for x in info[4]]
-            mount = Mountain(name, height, difficulty, routelen, diffs)
-            self.mounts.append(mount)
-            
 
 class Game:
     def __init__(self):
         self._running = True
         self._display_surf = None
-        self._image_surf = None
-        self.newImage = image('ascensionopenscreen.png')
+        self.newImage = None
  
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((1366,768), pygame.HWSURFACE)
         self._running = True
-        cwd = os.getcwd()
-        self._image_surf = pygame.image.load(os.path.join(cwd, 'ascensionopenscreen.png')).convert_alpha()
-        self._image_surf = pygame.transform.scale(self._image_surf, (1360,760))
-        beginner = level("beginning")
-        beginner.on_init();
+        self.newImage = displaylib.image("ascensionopenscreen.png")
+        self.newImage._image_surf = pygame.transform.scale(self.newImage._image_surf, (1360,760))
+        beginner = AsscensionLib.level("beginning")
+        beginner.on_init()
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -64,7 +23,7 @@ class Game:
     def on_loop(self):
         pass
     def on_render(self):
-        self._display_surf.blit(self._image_surf,(0,0))
+        self._display_surf.blit(self.newImage._image_surf,(0,0))
         pygame.display.flip()
  
     def on_cleanup(self):
